43c3748f8ceb95cc623e4394bc58b4c8014a6286
shuang.song <shuang.song@tqmall.com>
2015-12-27 19:38:06 +0800

    app.js

--- app.js
+++ app.js
@@ -4,12 +4,14 @@
 
 var http = require('http');
 
-http.createServer(function (request, response) {
+var server = http.createServer(function (request, response) {
     response.writeHead(200, {"Content-Type": "text/plain"});
     response.write("Hello Docker");
     response.end();
-}).listen(3333);
+});
+
+server.listen(3333);
 
 setTimeout(function () {
-    http.close();
+   server.close();
 }, 5000);
