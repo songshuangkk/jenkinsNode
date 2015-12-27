2c9eea55d12d56c005dbf926be1f9278a45a4166
shuang.song <shuang.song@tqmall.com>
2015-12-27 19:31:42 +0800

    app.js

---
+++ app.js
@@ -0,0 +1,11 @@
+/**
+ * Created by songshuang on 15/12/27.
+ */
+
+var http = require('http');
+
+http.createServer(function (request, response) {
+    response.writeHead(200, {"Content-Type": "text/plain"});
+    response.write("Hello Docker");
+    response.end();
+}).listen(3000);
