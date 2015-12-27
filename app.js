954f7953553c996cf91630bed7459d09bb54447d
shuang.song <shuang.song@tqmall.com>
2015-12-27 20:45:05 +0800

    app.js

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var http = require('http');
 
 var server = http.createServer(function (request, response) {
     response.writeHead(200, {"Content-Type": "text/plain"});
-    response.write("Hello Docker");
+    response.write("Hello Word");
     response.end();
 });
 
